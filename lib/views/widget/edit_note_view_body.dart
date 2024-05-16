c762295494b3a925eb92761d565c6eeceefa1495
mahmoud <87919952+Mahmoud14799@users.noreply.github.com>
2024-05-16 04:43:33 +0300

    add edit note view ind note view body

---
+++ lib/views/widget/edit_note_view_body.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:note_pro/views/widget/custom_text_fild.dart';
+import 'package:note_pro/views/widget/custtom_app_bar.dart';
+
+class EditNoteViewBody extends StatelessWidget {
+  const EditNoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          CustomAppBar(title: 'Edit Note',icon: Icons.done),
+          SizedBox(
+            height: 30,
+          ),
+          CustomTextField(hint: 'title'),
+          SizedBox(
+            height: 15,
+          ),
+          CustomTextField(
+            hint: 'Content',
+            maxLine: 4,
+          )
+        ],
+      ),
+    );
+  }
+}

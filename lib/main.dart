bf71b09a62cbdefec4d5d65a94522e095155efac
mahmoud <87919952+Mahmoud14799@users.noreply.github.com>
2024-06-17 21:09:31 +0300

    create constants file and init hive and open Box hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_pro/constants.dart';
 import 'package:note_pro/views/note_view.dart';
 
-void main() {
+void main() async {
+
+await Hive.initFlutter();
+await Hive.openBox(kNotesBox);
   runApp(const NoteApp());
 }
 

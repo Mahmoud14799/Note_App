29b3964bdf4374c452878b96f29025484f1c8d60
mahmoud <87919952+Mahmoud14799@users.noreply.github.com>
2024-06-19 21:56:17 +0300

    add cubit to button sheet

--- lib/views/widget/add_note_button_sheet.dart
+++ lib/views/widget/add_note_button_sheet.dart
@@ -1,22 +1,50 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_pro/cubit/add_note_cubit/add_note_cubit.dart';
 import 'package:note_pro/views/widget/add_note_form.dart';
-import 'package:note_pro/views/widget/custom_button_sheet.dart';
-import 'package:note_pro/views/widget/custom_text_fild.dart';
+
+
 
 class AddNoteButtonSheet extends StatelessWidget {
   const AddNoteButtonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-          padding: EdgeInsets.only(
-              right: 16,
-              left: 16,
-              bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: const AddNoteForm()),
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: SingleChildScrollView(
+        child: Padding(
+            padding: EdgeInsets.only(
+                right: 16,
+                left: 16,
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: BlocConsumer<AddNoteCubit, AddNoteState>(
+              listener: (context, state) {
+                if (state is AddNoteSuccess) {
+                  Navigator.pop(context);
+                  AwesomeDialog(
+                    context: context,
+                    dialogType: DialogType.success,
+                    title: 'Done',
+                    desc: 'Note Added',
+                    btnOkText: 'Ok',
+                  );
+                }
+
+                if (state is AddNoteError) {
+                  state.message;
+                }
+              },
+              builder: (context, state) {
+                return ModalProgressHUD(
+                    inAsyncCall: state is AddNoteLoading ? true : false,
+                    child: const AddNoteForm());
+              },
+            )),
+      ),
     );
   }
 }
-
